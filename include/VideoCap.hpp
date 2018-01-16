3d8d966d1cb9af8c8eb0c301729f7dc418efd570
David <dhen2714@uni.sydney.edu.au>
2018-01-16 16:14:57 +1100

    Separate r/w thread implementation.

--- include/VideoCap.hpp
+++ include/VideoCap.hpp
@@ -1,7 +1,12 @@
 /*
 Capture application and VideoCapture object for displaying and writing frames
 from Leopard OV-580 stereo camera. Commands can be entered via the command
-line after the application starts.
+line after the application starts. Commands are:
+    start - Starts writing frames continuously to disk.
+    'n'   - Where 'n' is an integer; writes 'n' frames to disk.
+    stop  - Stops writing. Can be used after one of the previous two commands
+            are called.
+    q     - Quits application.
 
 Capture Application initializes video capture device with address /dev/video0.
 
@@ -32,7 +37,7 @@ extern "C" {
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
-
+// V4L2 - video4linux
 #include <linux/videodev2.h>
 }
 
@@ -41,6 +46,13 @@ extern "C" {
 #include <atomic>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+
+#include <boost/circular_buffer.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/call_traits.hpp>
+#include <boost/bind.hpp>
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
@@ -70,10 +82,10 @@ private:
     void errno_exit(const char *s);
     int xioctl(int fh, int request, void *arg);
 
-    void open_device(); // Open call on file descriptor.
+    void open_device(); // 'open()' call on file descriptor.
     void init_device(); // Sets video capture format, fps, calls init_mmap().
     void init_mmap(); // Initiates memory mapping.
-    void start_capturing(); // Starts capture.
+    void start_capturing(); // Starts capture, queues buffers.
     int process_frame(cv::Mat *frame); // Reads buffer into frame.
     void uninit_device(); // Unitiates memory map.
     void close_device(); // Closes device.
@@ -116,18 +128,61 @@ public:
     struct timeval timestamp;
 };
 
+class bounded_buffer
+/*
+Thread-safe implementation of circular buffer, ensuring mutual exclusion
+between capture and write threads during buffer access. This is achieved by
+wrapping the operations of the underlying boost::circular_buffer object with
+lock acquisition and release.
+
+Implementation is based on this example:
+http://www.boost.org/doc/libs/master/libs/circular_buffer/example/circular_buffer_bound_example.cpp
+*/
+{
+public:
+    typedef boost::circular_buffer<cv::Mat> container_type;
+    typedef typename container_type::size_type size_type;
+    typedef typename container_type::value_type value_type;
+    typedef typename boost::call_traits<value_type>::param_type param_type;
+
+    explicit bounded_buffer(size_type capacity)
+    : m_unread(0), m_container(capacity) {}
+
+    void push_front(typename boost::call_traits<value_type>::param_type item);
+
+    void pop_back(value_type* frameCopy);
+
+private:
+    // Disabled copy constructor.
+    bounded_buffer(const bounded_buffer&);
+    // Disabled assign operator.
+    bounded_buffer& operator = (const bounded_buffer&);
+
+    bool is_not_empty() const { return m_unread > 0; }
+    bool is_not_full() const { return m_unread < m_container.capacity(); }
+
+    size_type m_unread;
+    container_type m_container;
+    boost::mutex m_mutex;
+    boost::condition m_not_empty;
+    boost::condition m_not_full;
+};
+
 class CaptureApplication
 {
 private:
     VideoCapture vc;
-    std::thread captureThread;
+    std::thread readThread; // Thread for reading frames from VideoCapture.
+    std::thread writeThread; // Thread for writing frames from VideoCapture.
+    bounded_buffer *CapAppBuffer; // Circular buffer for frame R/W.
     std::atomic_bool writeContinuous; // Switch for writing frames to disk.
     std::atomic_bool writeSingles; // Switch for writing given amount of frames.
     std::atomic_bool writing; // Write status.
     std::atomic_bool captureOn; // Video capture switch.
     std::atomic_ulong writeCount; // Number of frames written to disk.
     std::atomic_uint additionalFrames; // User specified number of frames.
-    cv::Mat frame = cv::Mat(480, 1280, CV_8U);
+    cv::Mat frame; // OpenCV Mat object which camera buffer is read to.
+    const unsigned int cap_app_size = 500; // Capacity of circular buffer.
 
     void run_capture(); // Loops through Videocapture.read() calls.
     void parse_command();
@@ -138,8 +193,8 @@ private:
     void get_write_status();
     bool numeric_command(const std::string *command); // Checks if input is num.
     unsigned int str2int(const std::string *command); // Converts str to int.
-
-
+    void read_frames(); // Reads frames to CapAppBuffer, and displays them.
+    void write_frames(); // Writes frames from CapAppBuffer.
 public:
     CaptureApplication();
     ~CaptureApplication();
--- source/CapApp.cpp
+++ source/CapApp.cpp
@@ -1,22 +1,38 @@
 #include <VideoCap.hpp>
 
 CaptureApplication::CaptureApplication()
-: writeContinuous(false), writeSingles(false), captureOn(true), writeCount(0)
+: writeContinuous(false), writeSingles(false), captureOn(true), writeCount(0),
+frame(cv::Mat(480, 1280, CV_8U))
 {
+    // Print out current fps.
     std::cout << "FPS: " << vc.get_fps() << std::endl;
-    writing = (writeContinuous || writeCount);
+    writing = (writeContinuous || writeCount); // Initial write status = 0.
     get_write_status();
+    /* Because the bounded buffer could not be initialized directly as a class
+    variable, we initialize the CapAppBuffer pointer instead, and set it to
+    the address of the bounded buffer buf. */
+    bounded_buffer buf(cap_app_size);
+    CapAppBuffer = &buf;
 
-    captureThread = std::thread(&CaptureApplication::run_capture, this);
+    // Start separate threads for reading and writing frames to/from the
+    // application buffer.
+    readThread = std::thread(&CaptureApplication::read_frames, this);
+    writeThread = std::thread(&CaptureApplication::write_frames, this);
+
     while (captureOn) {
         parse_command();
     }
-    captureThread.join();
+    // When captureOn is set to false via the 'q' command, end application.
+    readThread.join();
+    std::cout << "..." << std::endl;
+    writeThread.join();
+    std::cout << "..." << std::endl;
 }
 
 CaptureApplication::~CaptureApplication()
 {
     vc.release();
+    std::cout << "Application exited." << std::endl;
 }
 
 bool CaptureApplication::numeric_command(const std::string *command)
@@ -52,9 +68,12 @@ void CaptureApplication::parse_command()
     std::string command;
     std::cin >> command;
     if (command == "q") {
+        // Quit command.
         std::cout << "Quitting..." << std::endl;
         captureOn = false;
+        std::cout << "..." << std::endl;
     } else if (command == "start" && !writing) {
+        // Starts writing frames to disk continuously.
         writeContinuous = true; update_write_status();
         std::cout << "Writing frames..." << std::endl;
     } else if (command == "start" && writing) {
@@ -62,6 +81,7 @@ void CaptureApplication::parse_command()
     } else if (command == "stop" && !writing) {
         std::cout << "Enter 'start' to commence writing" << std::endl;
     } else if (command == "stop" && writing) {
+        // Stops writing frames to disk.
         writeContinuous = false; writeSingles = false;
         std::cout << "Write stopped!" << std::endl;
         update_write_status();
@@ -70,21 +90,68 @@ void CaptureApplication::parse_command()
         std::cout << "Writing " << additionalFrames
                   << " frames" << std::endl;
         writeSingles = true; update_write_status();
+    } else if (numeric_command(&command) && writing) {
+        std::cout << "Already writing!" << std::endl;
     } else if (command == "fps") {
         captureOn = false;
-        captureThread.join();
+        readThread.join();
+        writeThread.join();
         vc.release();
         vc.capture(true);
         captureOn = true;
-        captureThread = std::thread(&CaptureApplication::run_capture, this);
+        readThread = std::thread(&CaptureApplication::read_frames, this);
+        writeThread = std::thread(&CaptureApplication::write_frames, this);
         get_write_status();
     } else {
         std::cout << "Command not valid!" << std::endl;
     }
 }
 
+void CaptureApplication::read_frames()
+{
+    int ret;
+    while (captureOn)
+    {
+        frame.data == nullptr;
+        ret = vc.read(&frame);
+        if (ret) {
+            CapAppBuffer->push_front(frame); // writes to front of buffer
+            cv::imshow("Frame", frame);
+            cv::waitKey(1);
+        }
+    }
+    cv::destroyAllWindows();
+}
+
+void CaptureApplication::write_frames()
+{
+    // Mat objects held in buffer are written to frameCopy.
+    cv::Mat frameCopy(480, 1280, CV_8U);
+    while (captureOn)
+    {
+        CapAppBuffer->pop_back(&frameCopy);
+        if (writeContinuous) {
+            write_image(&frameCopy);
+            writeCount += 1;
+        } else if (writeSingles) {
+            if (additionalFrames > 0) {
+                write_image(&frameCopy);
+                writeCount += 1;
+                --additionalFrames;
+            } else {
+                writeSingles = false;
+                update_write_status();
+            }
+        }
+    }
+}
+
 void CaptureApplication::run_capture()
 {
+    // This is the old method in which read and write calls were on the
+    // same thread. This lead instances where writing frames took longer than
+    // time interval between vc.read() calls, causing stutters in the video
+    // feed.
     int ret;
     while (captureOn) {
         frame.data == NULL;
@@ -99,7 +166,8 @@ void CaptureApplication::run_capture()
                     writeCount += 1;
                     --additionalFrames;
                 } else {
-                    writeSingles = false; update_write_status();
+                    writeSingles = false;
+                    update_write_status();
                 }
             }
             cv::imshow("Frame", frame);
@@ -161,4 +229,26 @@ void CaptureApplication::print_timestamp()
     curtime = tv.tv_sec;
     strftime(ts, 30, "%m-%d-%Y  %T.",localtime(&curtime));
     printf("%s%ld\n",ts,tv.tv_usec);
+}
+
+void bounded_buffer::push_front(
+    typename boost::call_traits<value_type>::param_type item)
+{
+    // `param_type` represents the "best" way to pass a parameter of type
+    // `value_type` to a method.
+    boost::mutex::scoped_lock lock(m_mutex);
+    m_not_full.wait(lock, boost::bind(&bounded_buffer::is_not_full, this));
+    m_container.push_front(item);
+    ++m_unread;
+    lock.unlock();
+    m_not_empty.notify_one();
+}
+
+void bounded_buffer::pop_back(value_type* frameCopy)
+{
+    boost::mutex::scoped_lock lock(m_mutex);
+    m_not_empty.wait(lock, boost::bind(&bounded_buffer::is_not_empty, this));
+    *frameCopy = m_container[--m_unread];
+    lock.unlock();
+    m_not_full.notify_one();
 }
